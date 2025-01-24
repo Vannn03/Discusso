0d30a5061733d214e867904d9ba9373a00eeaf7a
Vannn03 <90308438+Vannn03@users.noreply.github.com>
2025-01-24 23:10:03 +0700

    add: page redesign

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -6,16 +6,36 @@ import LogoutButton from "@components/buttons/LogoutButton";
 const Settings = () => {
     const loc = useLocation();
     return (
-        <div className="d-flex gap-4">
-            <ListGroup defaultActiveKey={loc.pathname} className="w-25">
-                <ListGroup.Item action href="/settings/profile">
-                    Profile
-                </ListGroup.Item>
-                <LogoutButton />
-                <DeleteAccountButton />
-            </ListGroup>
-            <div className="border-start ps-4">
-                <Outlet />
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-4 col-lg-3">
+                    <div className="card shadow-sm border-0">
+                        <div className="card-body">
+                            <ListGroup
+                                variant="flush"
+                                defaultActiveKey={loc.pathname}
+                            >
+                                <ListGroup.Item
+                                    action
+                                    href="/settings/profile"
+                                    className="py-3"
+                                >
+                                    Profile
+                                </ListGroup.Item>
+                                <LogoutButton />
+                                <DeleteAccountButton />
+                            </ListGroup>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-8 col-lg-9">
+                    <div className="card shadow-sm border-0">
+                        <div className="card-body">
+                            <Outlet />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
